36f1770394e5f7ed7a6a0c5995a33b90c427849d
ksw0154 <rlatkddnjs44@naver.com>
2021-12-15 19:50:50 +0900

    기능 구현 완성

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,15 +2,28 @@ import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+
+      // onSubmit event는 페이지를 새로고침 시키기 때문에 e.preventDefault()를 사용한다.
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    // onClick event로 충분히 처리할 수 있다.
+    // onSubmit을 사용했을 때 Enter 키를 사용할 수 있게 된다.
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
